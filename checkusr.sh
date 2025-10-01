f2803319a859f873893a6c7ce92db29e9efbadb2
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-10-01 13:00:37 +0530

    Create checkusr.sh

---
+++ checkusr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter username to check: " USERNAME
+
+if id "$USERNAME" &>/dev/null; then
+    echo "User $USERNAME exists."
+else
+    echo "User $USERNAME does not exist."
+fi
