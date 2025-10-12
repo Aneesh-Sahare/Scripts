47a6c76c92528039eb5fa90310fec70a2914b37b
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-10-12 20:12:48 +0530

    Update disku.sh

--- disku.sh
+++ disku.sh
@@ -4,7 +4,7 @@ THRESHOLD=80
 USAGE=$(df / | tail -1 | awk '{print $5}' | sed 's/%//')
 
 if [ $USAGE -gt $THRESHOLD ]; then
-    echo "Disk usage is above $THRESHOLD% ($USAGE%). Take action!"
+    echo "Disk usage is above $THRESHOLD ($USAGE). Take action!"
 else
-    echo "Disk usage is normal ($USAGE%)."
+    echo "Disk usage is normal ($USAGE)."
 fi
