0912e1b535a389b3eeb023f9a07076385eb6c5a8
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-10-18 20:26:58 +0530

    Clarify package installation script with comment
    Added a comment to clarify the purpose of the script.

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for package in $(cat install-packages.txt)
+for package in $(cat install-packages.txt)            #create a file install-package.txt and put all the name of the packages into it. 
 do
   sudo apt-get -y install $package
 done
