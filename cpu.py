5b06b1271e456007b24255ec517ea6c7b4fd52b9
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-10-19 12:16:15 +0530

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -1,4 +1,9 @@
 import psutil
-cpu = psutil.cpu_percent()
-if cpu > 80:
-    print(" High CPU usage detected!")
+import time
+
+while True:
+    cpu = psutil.cpu_percent(interval=1)
+    print(f"CPU Usage: {cpu}%")
+    if cpu > 80:
+        print(" High CPU usage detected!")
+    time.sleep(1)
