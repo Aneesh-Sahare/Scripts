ec4fba6ade5770f2c9440293e80d7de1911d40f1
Aneesh Sahare <145126838+Aneesh-Sahare@users.noreply.github.com>
2025-10-28 18:53:45 +0530

    just a game . jus4fun

---
+++ guess.py
@@ -0,0 +1,13 @@
+
+print ("Thanks for playing this game!, Hope you enjoy it")
+
+guess=int(input("Guess the number:"))
+
+answer=444
+
+while answer!=guess:
+  guess=int(input("Guess another number:"))
+  
+else:
+  print("you got the right number")
+  
